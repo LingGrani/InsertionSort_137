a521aec864cd1857e0288b1d167d9588ed2ea11c
LingGrani <luthfiangger2510@gmail.com>
2023-03-23 15:16:47 +0700

    prosdure insertionsort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -36,7 +36,24 @@ void insertionsort()
 {
 	int temp; //variable data temporer / penyimpanan sementara
 	int j; // variable J sebagai penanda
+
+	for (int i = 1; i < n; i++) //1. looping dengan i dimulai dari 1 hingga n - 1
+	{
+		temp = arr[i]; //2. simpan nilai arr[i] ke variabel sementara temp
+		j = i - 1; //3. setting nilai j sama dengan i-1
+		while (j >= 0 && arr[j] > temp) //4. looping while dimana nilai j lebih besar sama dengan 0 dan array[j] lebih besar dari temp
+		{
+			arr[j - 1] = arr[j];// simpan arr[j] kedalam variable arr[j+1]
+			j--; // j decrement
+		}
+		arr[j + 1] = temp;
+		arr[j + 1] = temp; // 5. simpan nilai temp ke dalam arr[j+1]
+		cout << endl;
+		cout << "\npass " << i << ":"; // output ke layar
+		
+	}
 }
+
 int main()
 {
    
